2309c697b5a76121c2a714ceafa18935cf57a102
Krystian Grabowy <krys9802@gmail.com>
2023-06-15 20:43:42 +0200

    MVVM design pattern implemented

---
+++ MockingDemo/Models/LoginResponse.swift
@@ -0,0 +1,12 @@
+//
+//  LoginResponse.swift
+//  MockingDemo
+//
+//  Created by Krystian Grabowy on 15/06/2023.
+//
+
+import Foundation
+
+struct LoginResponse: Codable {
+    let success: Bool
+}
---
+++ MockingDemo/Views/DashboardView.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardView.swift
+//  MockingDemo
+//
+//  Created by Krystian Grabowy on 15/06/2023.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+            .navigationTitle("Dashboard")
+        }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
