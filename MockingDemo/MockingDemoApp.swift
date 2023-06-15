bc1f7b9654127dce83a7671927c6eec7bd6d40bc
Krystian Grabowy <krys9802@gmail.com>
2023-06-15 20:28:34 +0200

    Initial Commit

---
+++ MockingDemo/MockingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockingDemoApp.swift
+//  MockingDemo
+//
+//  Created by Krystian Grabowy on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MockingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
